c4e6058ab1bbe41a6064032ba520ed07690e5de8
Himanshu Purohit <himanshu1203@gmail.com>
2023-09-06 15:58:59 +0530

    DB: User database schema

--- server/utils/database.js
+++ server/utils/database.js
@@ -1,26 +1,25 @@
 import mongoose from "mongoose";
-
 let isConnected = false;
 
 export const connectToDb = async () => {
-    mongoose.set('strictQuery', true);
+  console.log(process.env.MONGODB_URI);
+  mongoose.set("strictQuery", true);
 
-    if(isConnected) {
-        console.log("MongodDB is already connected!");
-        return;
-    }
+  if (isConnected) {
+    console.log("MongodDB is already connected!");
+    return;
+  }
 
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: "share_prompt",
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "connect_hub",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-        isConnected  = true;
-        console.log("MongoDB Connected");
-
-    } catch (error) {
-        console.log(error);
-    }
-}
+    isConnected = true;
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
