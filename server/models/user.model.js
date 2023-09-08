2f129deb96497dbfa2e6e6769118e85bdb214e8a
Himanshu Purohit <himanshu1203@gmail.com>
2023-09-08 12:24:37 +0530

    REFACTOR: Refectoring user schema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,48 +1,38 @@
 import mongoose from "mongoose";
 
 // Define a User schema
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true, // Ensures usernames are unique
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true, // Ensures email addresses are unique
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  profilePicture: {
-    type: String, // Store the file path or URL to the profile picture
-  },
-  bio: {
-    type: String,
-  },
-  // Add fields for OAuth2 authentication (if applicable)
-  oauthProviders: [
-    {
-      provider: String, // e.g., 'google', 'facebook'
-      providerId: String, // ID from the provider
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-  ],
-  // Timestamps to track when the user document was created and updated
-  createdAt: {
-    type: Date,
-    default: Date.now,
+    username: {
+      type: String,
+      required: true,
+      unique: true, // Ensures usernames are unique
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true, // Ensures email addresses are unique
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    // Add fields for OAuth2 authentication (if applicable)
+    oauthProviders: [
+      {
+        provider: String, // e.g., 'google', 'facebook'
+        providerId: String, // ID from the provider
+      },
+    ],
   },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 // Create a User model
 const User = mongoose.model("User", userSchema);
