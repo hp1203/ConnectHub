c1cd0338a02344743346f86c38d758cc2b431f7c
Himanshu Purohit <himanshu1203@gmail.com>
2023-09-08 16:30:17 +0530

    FIX: Refectoring user controller

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,15 +1,43 @@
 import { renameSync } from "fs";
 import { connectToDb } from "../utils/database.js";
 import User from "../models/user.model.js";
+import Profile from "../models/profile.model.js";
+
+export const getUserInfo = async (request, response) => {
+
+}
+
+export const setUserProfile = async (request, response) => {
+  connectToDb();
+  try {
+    const { title, bio } = request.body;
+    const { userId } = response;
+    const profile = await Profile.create({
+      userId,
+      profileTitle: title,
+      bio
+    });
+
+    return response.status(201).json({
+      profile: profile,
+      success: true,
+      message: "Profile created successfully!",
+    });
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({ error: error.message });
+  }
+}
 
 export const updateProfile = async (request, response) => {
   connectToDb();
 
   try {
-    const { userId, email } = response;
+    const { userId } = response;
+    const { profileId } = request.params;
 
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: userId, email: email },
+    const updatedUser = await Profile.findOneAndUpdate(
+      { _id: profileId, userId: userId },
       request.body,
       { new: true, upsert: true }
     );
@@ -29,6 +57,7 @@ export const updateProfileImage = async (request, response) => {
   try {
     const { file } = request;
     const { userId, email } = response;
+    const { profileId } = request.params;
 
     if (!file) return response.status(400).json({ error: "Image is required" });
 
@@ -36,13 +65,14 @@ export const updateProfileImage = async (request, response) => {
     let fileName = "uploads/profiles/" + date + request.file.originalname;
     renameSync(request.file.path, fileName);
 
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: userId, email: email },
+    const updatedProfile = await Profile.findOneAndUpdate(
+      { _id: profileId, userId: userId },
       { profilePicture: fileName },
       { new: true, upsert: true }
     );
+
     return response.status(201).json({
-      user: updatedUser,
+      user: updatedProfile,
       success: true,
       message: "Profile updated successfully!",
     });
--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import { updateProfile, updateProfileImage } from "../controllers/UserController.js";
+import { setUserProfile, updateProfile, updateProfileImage } from "../controllers/UserController.js";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
 import multer from "multer";
 
 const userRoutes = Router();
 const upload = multer({ dest: "uploads/profiles" });
-// userRoutes.get("/profile/:id", getUserProfile);
-userRoutes.put("/profile", verifyToken, updateProfile);
-userRoutes.post("/profile/image", verifyToken, upload.single("images"), updateProfileImage);
+
+userRoutes.post("/profile", verifyToken, setUserProfile);
+userRoutes.put("/profile/:profileId", verifyToken, updateProfile);
+userRoutes.post("/profile/:profileId/image", verifyToken, upload.single("images"), updateProfileImage);
 
 export default userRoutes;
