18c13e51c816dff5ee4178cd6d3c507f16eeea22
Himanshu Purohit <himanshu1203@gmail.com>
2023-09-13 17:23:45 +0530

    UI: Frontend footer

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import "../styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header/>
         {children}
+        <Footer/>
       </body>
     </html>
   )
