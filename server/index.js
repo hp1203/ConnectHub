7331ab81af9ce097951ccba511199305acb89c83
Himanshu Purohit <himanshu1203@gmail.com>
2023-09-19 11:49:20 +0530

    FIX: Added frontend to cors

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: [process.env.ORIGIN],
+    origin: [process.env.ORIGIN, 'http://localhost:3000'],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -5,7 +5,6 @@ const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
     },
     username: {
       type: String,
