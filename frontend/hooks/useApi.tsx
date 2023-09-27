365336ba7fd6149a88ddc6217f89e2fba3c01f53
Himanshu Purohit <himanshu1203@gmail.com>
2023-09-28 01:41:55 +0530

    FIX: Making useApi server side

--- frontend/hooks/useApi.tsx
+++ frontend/hooks/useApi.tsx
@@ -1,53 +1,27 @@
-"use client";
 import axios, { AxiosResponse } from "axios";
-import { useState } from "react";
-
-interface ApiResponse {
-  data: any;
-  status: number;
-  statusText: string;
-  headers: any;
-  config: any;
-  request?: any;
-}
 
 const useApi = (auth_token = "") => {
   const uri = process.env.NEXT_PUBLIC_API_URL;
-  const [response, setResponse] = useState<ApiResponse | null>(null);
-  const [error, setError] = useState<any>(null);
-  const [loading, setLoading] = useState<boolean>(false);
   const header = {
     "Content-Type": "application/json",
     "Authorization": `Bearer ${auth_token}`,
   };
 
-  const fetchData = (
+  const fetchData = async (
     method: "get" | "post" | "put" | "delete",
     url: string,
     data?: any
   ) => {
-    setLoading(true);
-    axios({
+    return axios({
       method,
       url: uri + url,
       data,
       headers: header
     })
-      .then((data: AxiosResponse) => {
-        setResponse(data);
-        setLoading(false);
-      })
-      .catch((error: any) => {
-        setError(error.response.data);
-        setLoading(false);
-      });
   };
 
   return {
     fetchData,
-    response,
-    loading,
-    error,
   };
 };
 
