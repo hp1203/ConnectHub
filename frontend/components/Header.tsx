6e82862e8faae2d84fd45d92a82ec5a93cf65615
Himanshu Purohit <himanshu1203@gmail.com>
2023-10-13 20:04:56 +0530

    FIX: Signin and Signup Link

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -44,10 +44,10 @@ const Header: React.FC = () => {
         </nav>
 
         <div className="hidden md:flex items-center justify-end">
-          <Link href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+          <Link href="/auth" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
             Sign in
           </Link>
-          <Link href="#" className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-500 hover:bg-blue-600 transition duration-150">
+          <Link href="/auth/register" className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-500 hover:bg-blue-600 transition duration-150">
             Sign up
           </Link>
         </div>
