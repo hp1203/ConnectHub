13dd49055ac90b56ae647ff814983589a63be796
Himanshu Purohit <himanshu1203@gmail.com>
2023-10-25 16:00:58 +0530

    FNC: Edit social links setting DONE

--- frontend/app/admin/settings/page.tsx
+++ frontend/app/admin/settings/page.tsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react";
 import EditDisclosure from "@/components/Dashboard/EditDisclosure";
 import EditFont from "@/components/Dashboard/EditFont";
 import EditProfileInfo from "@/components/Dashboard/EditProfileInfo";
+import EditSocialMedia from "@/components/Dashboard/EditSocialMedia";
 
 const Settings = () => {
   const { data: session } = useSession();
@@ -46,6 +47,7 @@ const Settings = () => {
       <div className="h-screen grid grid-cols-1 md:grid-cols-5 gap-6 w-full">
         <div className="flex flex-1 flex-col gap-4 col-span-3 overflow-y-scroll scrollbar-hide p-1">
           <EditProfileInfo title={profile?.profileTitle} url={profile?.url} description={profile?.bio}/>
+          <EditSocialMedia {...profile?.socialLinks}/>
         </div>
         <div className="col-span-2 mt-1">
           <Card title="Preview" className="overflow-y-scroll scrollbar-hide">
