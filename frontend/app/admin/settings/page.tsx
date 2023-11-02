3d8cd2fb9b24dc38ae2cea46b0b7694f1936fde0
Himanshu Purohit <himanshu1203@gmail.com>
2023-11-02 17:50:57 +0530

    FIX: Fixing loader

--- frontend/app/admin/settings/page.tsx
+++ frontend/app/admin/settings/page.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 import EditProfileInfo from "@/components/Dashboard/EditProfileInfo";
 import EditSocialMedia from "@/components/Dashboard/EditSocialMedia";
 import EditProfileImage from "@/components/Dashboard/EditProfileImage";
+import Loading from "@/components/Dashboard/Loading";
 
 const Settings = () => {
   const { data: session } = useSession();
@@ -14,7 +15,7 @@ const Settings = () => {
 
   const [isLoading, setIsLoading] = useState(false);
   const [profile, setProfile] = useState<any>(null);
-  
+
   useEffect(() => {
     const fetchProfile = () => {
       setIsLoading(true);
@@ -27,8 +28,7 @@ const Settings = () => {
           console.log("Error", error);
           setIsLoading(false);
         });
-        console.log("pro",profile);
-        
+      console.log("pro", profile);
     };
 
     if (session?.token) {
@@ -43,11 +43,24 @@ const Settings = () => {
   return (
     <Content title="Settings">
       <div className="h-screen grid grid-cols-1 md:grid-cols-5 gap-6 w-full">
-        <div className="flex flex-1 flex-col gap-4 col-span-3 overflow-y-scroll scrollbar-hide p-1">
-          <EditProfileImage profilePicture={profile?.profilePicture}/>
-          <EditProfileInfo title={profile?.profileTitle} url={profile?.url} description={profile?.bio}/>
-          <EditSocialMedia {...profile?.socialLinks}/>
-        </div>
+        {isLoading ? (
+          <div className="flex flex-1 flex-col col-span-3 rounded-lg border border-gray-200 mt-1 items-center justify-center gap-3">
+            <Loading
+              title="Fetching Data..."
+              subtitle="Hold on! We're getting your settings."
+            />
+          </div>
+        ) : (
+          <div className="flex flex-1 flex-col gap-4 col-span-3 overflow-y-scroll scrollbar-hide p-1">
+            <EditProfileImage profilePicture={profile?.profilePicture} />
+            <EditProfileInfo
+              title={profile?.profileTitle}
+              url={profile?.url}
+              description={profile?.bio}
+            />
+            <EditSocialMedia {...profile?.socialLinks} />
+          </div>
+        )}
         <div className="col-span-2 mt-1">
           <Card title="Preview" className="overflow-y-scroll scrollbar-hide">
             <p>Preview Here</p>
--- frontend/app/admin/theme/page.tsx
+++ frontend/app/admin/theme/page.tsx
@@ -7,6 +7,7 @@ import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import EditDisclosure from "@/components/Dashboard/EditDisclosure";
 import EditFont from "@/components/Dashboard/EditFont";
+import Loading from "@/components/Dashboard/Loading";
 
 const Appearance = () => {
   const { data: session } = useSession();
@@ -35,27 +36,33 @@ const Appearance = () => {
     console.log("Theme", theme);
   }, [session]);
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
 
   return (
     <Content title="Edit Theme">
       <div className="h-screen grid grid-cols-1 md:grid-cols-5 gap-6 w-full">
-        <div className="flex flex-1 flex-col gap-4 col-span-3 overflow-y-scroll scrollbar-hide p-1">
-          <EditBackground
-            initialColor={theme?.background?.color}
-            initialOption={theme?.background?.bgType}
-            initialUrl={theme?.background?.url || ""}
-          />
-          <EditDisclosure
-            initialBgColor={theme?.disclosure.bgColor}
-            initialFontColor={theme?.disclosure.fontColor}
-            initialTitleColor={theme?.disclosure.titleColor}
-            initialHoverColor={theme?.disclosure.hoverColor}
-          />
-          <EditFont initialFontColor={theme?.font.color}/>
-        </div>
+        {isLoading ? (
+          <div className="flex flex-1 flex-col col-span-3 rounded-lg border border-gray-200 mt-1 items-center justify-center gap-3">
+            <Loading
+              title="Fetching Data..."
+              subtitle="Hold on! We're getting your datas."
+            />
+          </div>
+        ) : (
+          <div className="flex flex-1 flex-col gap-4 col-span-3 overflow-y-scroll scrollbar-hide p-1">
+            <EditBackground
+              initialColor={theme?.background?.color}
+              initialOption={theme?.background?.bgType}
+              initialUrl={theme?.background?.url || ""}
+            />
+            <EditDisclosure
+              initialBgColor={theme?.disclosure.bgColor}
+              initialFontColor={theme?.disclosure.fontColor}
+              initialTitleColor={theme?.disclosure.titleColor}
+              initialHoverColor={theme?.disclosure.hoverColor}
+            />
+            <EditFont initialFontColor={theme?.font.color} />
+          </div>
+        )}
         <div className="col-span-2 mt-1">
           <Card title="Preview" className="overflow-y-scroll scrollbar-hide">
             <p>Preview Here</p>
